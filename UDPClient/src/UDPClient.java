727a3325417d32aa1f17fe0100dd2780fb6a64e0
Naktirashu <bx4151gt@metrostate.edu>
2016-10-11 21:08:14 -0500

    Updated to echo client

--- UDPClient/src/UDPClient.java
+++ UDPClient/src/UDPClient.java
@@ -1,21 +1,55 @@
-import java.io.*;
+
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
 import java.net.*;
 
-class UDPClient {
-	public static void main(String args[]) throws Exception {
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		DatagramSocket clientSocket = new DatagramSocket();
-		InetAddress IPAddress = InetAddress.getByName("localhost");
-		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
-		String sentence = inFromUser.readLine();
-		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-		clientSocket.send(sendPacket);
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		clientSocket.receive(receivePacket);
-		String modifiedSentence = new String(receivePacket.getData());
-		System.out.println("FROM SERVER:" + modifiedSentence);
-		clientSocket.close();
+public class UDPClient {
+	
+	public GUI gui;
+	
+	public final static int PORT = 7;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		String hostname = "localhost";
+
+		if (args.length > 0) hostname = args[0];
+
+		try {
+
+			InetAddress ia = InetAddress.getByName(hostname);
+
+			DatagramSocket socket = new DatagramSocket();
+
+			SenderThread sender = new SenderThread(socket, ia, PORT);
+
+			sender.start();
+
+			Thread receiver = new ReceiverThread(socket);
+
+			receiver.start();
+
+		} catch (UnknownHostException ex) { System.err.println(ex);
+
+		} catch (SocketException ex) { System.err.println(ex);
+
+		}
+
 	}
+
 }
